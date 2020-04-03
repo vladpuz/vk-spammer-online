016878dce0834e11e3a1c04c0022d51ef4f7ee63
Владислав <vladislav.puzzyrev@gmail.com>
2020-04-03 16:28:36 +0500

    create-react-app

--- scripts/badBrowser.js
+++
@@ -1,7 +0,0 @@
-var ua = window.navigator.userAgent.toLowerCase();
-var is_ie = (/trident/gi).test(ua) || (/msie/gi).test(ua);
-if (is_ie) {
-    alert("Вы используете устаревший браузер!\n" +
-        "Наш сайт не работает на Internet Explorer.\n" +
-        "Скачайте один из современных браузеров: Chrome, Yandex, Firefox, Opera.")
-}
