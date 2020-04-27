8e9e557f8ea7135fdece1e7e0bcc6e06e7857c7a
Владислав <vladislav.puzzyrev@gmail.com>
2020-04-27 18:05:51 +0500

    added my proxy server

--- src/api/settings.ts
+++ src/api/settings.ts
@@ -1,3 +1,3 @@
 export const version = '5.89'
-export const proxyURL = 'https://cors-anywhere.herokuapp.com/'
+export const proxyURL = 'https://dry-lowlands-96591.herokuapp.com/'
 export const baseURL = proxyURL + 'https://api.vk.com/method/'
