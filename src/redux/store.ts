0eaf938e14de2e45c7eca5252e9f156e92d26ecc
Владислав <vladislav.puzzyrev@gmail.com>
2020-06-25 17:40:06 +0500

    Добавил ThunkType

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,7 +1,7 @@
-import { applyMiddleware, combineReducers, compose, createStore } from 'redux'
-import thunkMiddleware from 'redux-thunk'
-import accountsReducer from './ducks/accounts/reducer'
-import spamerReducer from './ducks/spamer/reducer'
+import { Action, applyMiddleware, combineReducers, compose, createStore } from 'redux'
+import thunkMiddleware, { ThunkAction } from 'redux-thunk'
+import accountsReducer from './ducks/accounts/accounts-reducer'
+import spamerReducer from './ducks/spamer/spamer-reducer'
 
 const reducers = combineReducers({
   accountsReducer,
@@ -12,6 +12,7 @@ const reducers = combineReducers({
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
 const store = createStore(reducers, composeEnhancers(applyMiddleware(thunkMiddleware)))
 
+export type ThunkType<R = void, A extends Action = Action> = ThunkAction<Promise<R>, StateType, unknown, A>
 export type GetActionsType<T> = T extends { [keys: string]: infer U } ? U : never
 export type StateType = ReturnType<typeof reducers>
 export default store
