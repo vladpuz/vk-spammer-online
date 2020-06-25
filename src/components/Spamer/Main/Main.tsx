8eb575bfd2c88c2fde1e230c0d67bbb970dace2e
Владислав <vladislav.puzzyrev@gmail.com>
2020-06-25 17:41:35 +0500

    Поменял структуру роутинга на массив

--- src/components/Spamer/Main/Main.tsx
+++ src/components/Spamer/Main/Main.tsx
@@ -1,14 +1,22 @@
 import React from 'react'
 import s from './Main.module.css'
-import { Route, Switch } from 'react-router-dom'
-import routes from '../../../config/routes'
+import { Route, Switch, Redirect } from 'react-router-dom'
+import routes from '../../../utils/routes'
 
 function Main () {
   return (
     <main className={s.main}>
       <Switch>
-        <Route path={routes.spamer.subroutes.accounts.path} component={routes.spamer.subroutes.accounts.component}/>
-        <Route path={routes.spamer.path} component={routes.spamer.component}/>
+        {
+          routes.map((route, index) => {
+            return route.redirect ? <Redirect key={index} to={route.redirect}/> : <Route
+              key={index}
+              path={route.path}
+              exact={route.exact}
+              render={props => <route.component {...props} routes={route.routes}/>}
+            />
+          })
+        }
       </Switch>
     </main>
   )
