5d4247d1c37425e176b9fe5bcef9d602ecf1ab66
Vladislav <vladislav.puzzyrev@gmail.com>
2022-07-17 00:54:27 +0500

    Удален блок поддержки

--- src/components/Spamer/Sidebar/Donate/Donate.tsx
+++ src/components/Spamer/Sidebar/Donate/Donate.tsx
@@ -5,15 +5,8 @@ import Title from '../../../common/Title/Title'
 function Donate () {
   return (
     <div className={s.donate}>
-      <Title>Поддержка</Title>
-      <iframe
-        src="https://money.yandex.ru/quickpay/shop-widget?writer=buyer&targets=&targets-hint=%D0%A7%D1%82%D0%BE%20%D0%B1%D1%8B%20%D0%B2%D1%8B%20%D1%85%D0%BE%D1%82%D0%B5%D0%BB%D0%B8%20%D1%87%D1%82%D0%BE%D0%B1%D1%8B%20%D1%8F%20%D0%B4%D0%BE%D0%B1%D0%B0%D0%B2%D0%B8%D0%BB%20%D0%B2%20%D1%81%D0%BF%D0%B0%D0%BC%D0%B5%D1%80%3F)&default-sum=50&button-text=11&hint=&successURL=https%3A%2F%2Fvladislav-puzyrev.github.io%2Fvk_spamer_online%2F&quickpay=shop&account=4100110035941487"
-        width="100%"
-        height="227"
-        frameBorder="0"
-        scrolling="no"
-        title="yandex-money"
-      />
+      <Title>Информация</Title>
+      <span>Проект больше не поддерживается, из-за отсутствия интереса</span>
     </div>
   )
 }
